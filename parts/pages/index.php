6cd7d604be91324b3d40ead4ac4d1a9993d18f5c
Craig Fowler <craig@wearegiants.com>
2015-11-06 16:20:51 -0800

    created splash page

--- parts/pages/index.php
+++ parts/pages/index.php
@@ -1,69 +1,22 @@
-
-<div class="wallpaper banner banner-medium fs-cell fs-full-all" data-background-options='{"source":{"0px":"/assets/IMG_1406-mobile.jpg","740px":"/assets/IMG_1406.jpg"}}'>
-	<div class="centered fs-all-full fs-contained">
+<div id="splash" class="covered">
+	<div class="centered centered-full">
 		<div class="fs-row">
-			<div class="fs-cell fs-all-full text-center">
-				<h2>Now available in the shop:<br> Skinnings in Knot White</h2>
-				<a class="btn btn-sm btn-link">Click Here</a>
+			<div class="fs-cell fs-lg-4 fs-md-half fs-sm-2 fs-centered text-center">
+				<div id="splash-content">
+					<img src="/assets/handmeups.jpg" class="img-responsive" alt="Hand Me Ups"/>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="splash-footer" class="pinned pinned-bottom">
+		<div class="fs-row">
+			<div class="fs-cell fs-lg-12 fs-md-6 fs-sm-3 fs-centered text-center">
+				<div id="splash-footer_content">
+					<div class="wrapper">
+						<p>We'll be back shortly. Visit us on <a href="#" target="blank">Instagram</a> in the meantime.</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-
-<hr class="invisible">
-
-<div class="fs-row">
-	<div class="fs-cell fs-lg-4 fs-md-3 fs-sm-3">
-		<img src="http://placehold.it/385x533/eeeeee/eeeeee" class="img-responsive">
-		<div class="meta text-center">
-			<h4>Stories</h4>
-			<h3>Call to action lorem ipsum</h3>
-		</div>
-	</div>
-	<div class="fs-cell fs-lg-8 fs-md-3 fs-sm-3">
-		<img src="http://placehold.it/800x533/eeeeee/eeeeee" class="img-responsive">
-		<div class="meta text-center">
-			<h4>Stories</h4>
-			<h3>Call to action lorem ipsum</h3>
-		</div>
-	</div>
-</div>
-
-<hr class="invisible">
-
-<section class="bg-color-bgGray">
-	<div class="fs-row">
-		<hr class="invisible fs-cell fs-full-all">
-		<header class="fs-cell fs-full-all text-center">
-			<h2>Timeline Skinnies</h2>
-		</header>
-		<div class="fs-cell fs-lg-fifth fs-md-fifth fs-sm-half"><a href="#"><img src="http://placehold.it/385x500/ffffff/ffffff" class="img-responsive"></a></div>
-		<div class="fs-cell fs-lg-fifth fs-md-fifth fs-sm-half"><a href="#"><img src="http://placehold.it/385x500/ffffff/ffffff" class="img-responsive"></a></div>
-		<div class="fs-cell fs-lg-fifth fs-md-fifth fs-sm-half"><a href="#"><img src="http://placehold.it/385x500/ffffff/ffffff" class="img-responsive"></a></div>
-		<div class="fs-cell fs-lg-fifth fs-md-fifth fs-sm-half"><a href="#"><img src="http://placehold.it/385x500/ffffff/ffffff" class="img-responsive"></a></div>
-		<div class="fs-cell fs-lg-fifth fs-md-fifth fs-sm-half"><a href="#"><img src="http://placehold.it/385x500/ffffff/ffffff" class="img-responsive"></a></div>
-		<hr class="invisible fs-cell fs-full-all">
-	</div>
-</section>
-
-<hr class="invisible fs-cell fs-full-all">
-
-<section class="fs-row">
-	<div class="fs-cell fs-all-full banner inverse bg-color-bgGray">
-		<div class="centered text-center">
-			<h2>Commercial Application <br>Lorem Ipsum Dolor Sit Amet</h2>
-			<a class="btn btn-sm btn-link">Click Here</a>
-		</div>
-	</div>
-</section>
-
-<hr class="invisible">
-
-<section class="fs-row">
-	<header class="fs-cell fs-full-all text-center">
-		<h2>Instagram</h2>
-	</header>
-	<div class="fs-cell fs-lg-third fs-md-third fs-sm-third"><a href="#"><img src="http://placehold.it/500x500/eeeeee/eeeeee" class="img-responsive"></a></div>
-	<div class="fs-cell fs-lg-third fs-md-third fs-sm-third"><a href="#"><img src="http://placehold.it/500x500/eeeeee/eeeeee" class="img-responsive"></a></div>
-	<div class="fs-cell fs-lg-third fs-md-third fs-sm-third"><a href="#"><img src="http://placehold.it/500x500/eeeeee/eeeeee" class="img-responsive"></a></div>
-</section>
