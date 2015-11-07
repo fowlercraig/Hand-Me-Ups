e92f0fc5a0af13f97e856fa33a467c5173e3af25
Craig Fowler <craig@wearegiants.com>
2015-11-06 16:42:48 -0800

    stuff

--- parts/pages/index.php
+++ parts/pages/index.php
@@ -8,15 +8,44 @@
 			</div>
 		</div>
 	</div>
+	<div id="splash-header" class="pinned pinned-top">
+		<div class="wrapper text-right">
+			<small><a class="swap swap-linked" data-swap-target="#about" data-swap-linked=".swap-linked" href="#">MORE INFO</a></small>
+		</div>
+	</div>
 	<div id="splash-footer" class="pinned pinned-bottom">
 		<div class="fs-row">
 			<div class="fs-cell fs-lg-12 fs-md-6 fs-sm-3 fs-centered text-center">
 				<div id="splash-footer_content">
 					<div class="wrapper">
-						<p>We'll be back shortly. Visit us on <a href="#" target="blank">Instagram</a> in the meantime.</p>
+						<p>We'll be back shortly. <br>Visit us on <a href="https://instagram.com/handmeups/" target="blank">Instagram</a> and <a href="https://www.facebook.com/handmeups.org" target="blank">Facebook</a> in the meantime.</p>
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>
+</div>
+
+<div id="about" class="covered covered-fixed bg-color-orange swap-overlay">
+	<div class="centered centered-full">
+		<div class="fs-row">
+			<div class="fs-cell fs-lg-7 fs-md-6 fs-sm-3 fs-centered text-center">
+				<hr class="invisible">
+				<div id="about-content">
+					<p class="emphasized color-white">
+						Hand Me Ups is an innovative and aspiring non-profit organization that provides new clothing to underprivileged youth.<br><br>
+						Hand Me Ups is a registered 501(c)(3) non-profit organization.<br><br>
+						Many of those in need today are used to receiving old, used, and often inferior hand-me-down clothing. Now, these same youth, outfitted in new, clean, and desirable branded clothing can experience an enhanced sense of self-worth and a new sense of purpose through our charitable donations and motivational presentations.<br><br>
+						Our goal is to help build self-esteem and confidence within these youth. Through our motivational mission we can help them realize a brighter future where they can set goals, implement plans, and turn their dreams into reality.
+					</p>
+					<hr class="invisible">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="about-footer" class="pinned pinned-top">
+		<div class="wrapper text-right">
+			<small><a href="#" class="swap swap-linked color-white" data-swap-target="#about" data-swap-linked=".swap-linked">CLOSE</a></small>
 		</div>
 	</div>
 </div>
